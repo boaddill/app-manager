9e2de107df1be85a3006a09ad9907bb143b6e8e0
boaddill <fernandogarciairun@gmail.com>
2018-05-21 19:59:13 +1200

    trabajando en modelos

--- fernando/apps/invoices/models.py
+++ fernando/apps/invoices/models.py
@@ -7,6 +7,7 @@ class Tasks (models.Model):
 	Project  	   = models.ForeignKey('projects.Project',on_delete=models.CASCADE , verbose_name='Project' )
 	
 class Item(models.Model):
+	code             = models.CharField("Code", max_length=200, blank=True,null=True )
 	item_name        = models.CharField("Item name", max_length=200, blank=True,null=True )
 	unit             = models.CharField("Units", max_length=200, blank=True,null=True )
 	item_price       = models.FloatField('Item price',default=None)
@@ -20,7 +21,15 @@ class Order(models.Model):
 
 
 class Docket(models.Model):
-	pass
+	code             = models.CharField("Code", max_length=200, blank=True,null=True )
+	date             = models.DateField('date',auto_now=False)
+	client           = models.ForeignKey('users.Profile_Client' ,on_delete=models.CASCADE ,blank=True,null=True, verbose_name='Client')
+
+
+
+	
+
+	
 
 
 class Invoice(models.Model):
@@ -37,17 +46,19 @@ class Invoice(models.Model):
 
 class Buying_Entry(models.Model):
 
+	
+	date             = models.DateField('date',auto_now=False)
 	item             = models.ForeignKey(Item , on_delete=models.CASCADE, verbose_name='Item' )
-	production_date  = models.DateField('date',auto_now=False)
 	units            = models.CharField("units", max_length=200, blank=True,null=True )
 	quantity		 = models.IntegerField()
-	project          = models.ForeignKey('projects.Project',on_delete=models.CASCADE , verbose_name='Project' )
-	task             = models.ForeignKey(Tasks, on_delete=models.CASCADE , verbose_name='TASKS' )
-	invoiced         = models.BooleanField(default=False,)
 	total_price      = models.FloatField()
-	invoice          = models.ForeignKey(Invoice,default=None ,on_delete=models.CASCADE , verbose_name='Invoice' )
-	order            = models.ForeignKey(Order,default=None ,on_delete=models.CASCADE , verbose_name='Order' )
-	docket           = models.ForeignKey(Docket,default=None ,on_delete=models.CASCADE , verbose_name='Docket' )
+	project          = models.ForeignKey('projects.Project',on_delete=models.CASCADE , verbose_name='Project' )
+	task             = models.ForeignKey(Tasks, on_delete=models.CASCADE , verbose_name='Task' )
+	provider         = models.ForeignKey('users.Profile_Provider', on_delete=models.CASCADE , verbose_name='Provider' )
+	invoiced         = models.BooleanField(default=False,)
+	invoice          = models.ForeignKey(Invoice,default=None ,on_delete=models.CASCADE , verbose_name='Invoice',related_name='invoice' )
+	order            = models.ForeignKey(Order,default=None ,on_delete=models.CASCADE , verbose_name='Order' ,related_name='order')
+	docket           = models.ForeignKey(Docket,default=None ,on_delete=models.CASCADE , verbose_name='Docket',related_name='docket' )
 
 
 
